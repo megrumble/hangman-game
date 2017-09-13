893bd4fdf5cee4d8cf52d6648a0316f716258cc5
Margaret Rumble <megrumble@gmail.com>
2017-09-13 10:23:22 -0400

    maskimg word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,28 @@
-var desserts =['apple pie','blueberry galette','chocolate donut','cupcakes','fruit tart', 'raspberry parfait',
+var letter =["a","b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var guess; //guess a letter
+var correct; //correct letter
+var wrong; //wrong letter
+var lives; //wrong guesses
+var choices =['apple pie','blueberry galette','chocolate donut','cupcakes','fruit tart', 'raspberry parfait',
 'strawberry shortcake', 'turtle brownie'];
+//randomly select word
 function chooseWord() {
-    return Math.floor(Math.random() * desserts.length);
-    
-
+return Math.floor(Math.random() * choices.length);
 }
-var word= desserts[chooseWord()];
+ var word=choices[chooseWord()];   
 console.log(word);
+//replace word with dashes
+
+for (var i = 0; i < word.length; i++){
+	word[i] = "_ ";
+}
+
+function printWord(){
+	for (var i = 0; i < word.length; i++){
+	var dashWord = document.getElementById("word");
+	var letter= document.createTextNode(word[i]);
+	dashWord.appendChild(letter);
+	}
+}
+
+ 
