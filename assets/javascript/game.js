574f31acb53129aae481015f3647102ee2079e4d
Margaret Rumble <megrumble@gmail.com>
2017-09-13 18:00:50 -0400

    this mess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,42 @@
-var letter =["a","b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var guess; //guess a letter
-var correct; //correct letter
-var wrong; //wrong letter
-var lives; //wrong guesses
-var choices =['apple pie','blueberry galette','chocolate donut','cupcakes','fruit tart', 'raspberry parfait',
-'strawberry shortcake', 'turtle brownie'];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var guess = ""; //guess a letter
+var display = ""; //correct letter-displayed
+var wrong = 0; //wrong guesses
+var lives = 6; //lives
+var choices = ['apple pie', 'blueberry galette', 'chocolate donut', 'cupcakes', 'fruit tart', 'raspberry parfait',
+    'strawberry shortcake', 'turtle brownie'
+];
 //randomly select word
 function chooseWord() {
-return Math.floor(Math.random() * choices.length);
+    return Math.floor(Math.random() * choices.length);
 }
- var word=choices[chooseWord()];   
+var word = choices[chooseWord()];
 console.log(word);
 //split word into characters
-
+var wordSplit = word.split("");
+return wordSplit;
 //replace characters with dashes
-function createMask(m)
-{
-mask = "";
-word.lenght = m.length;
- 
-for (i = 0; i < word.lenght; i ++)
-{
-mask += "_";
-}
-return mask;
- console.log(mask);
+function makeDashes() {
+    var dashArr = [];
+    for (i = 0; i <= wordSplit.length; i++) {
+        var dash = ["_"];
+        dashArr.push(dash);
+    }
+    //display hidden word
+    rightWordText.TextContent = dashArr;
+    return dashArr;
+
 }
 
-//display hidden word
+
+
+
+
+
+// user guesses a letter
+document.onkeyup = function (guess) {
+    for (i = 0; i <= letters.length; i++)
+
+}
+
+//
