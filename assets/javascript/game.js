34249cee3e7643b2274a6b2f0829dbd4159a6fa1
Margaret Rumble <megrumble@gmail.com>
2017-09-13 18:27:38 -0400

    at starbucks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,30 +3,45 @@ var guess = ""; //guess a letter
 var display = ""; //correct letter-displayed
 var wrong = 0; //wrong guesses
 var lives = 6; //lives
-var choices = ['apple pie', 'blueberry galette', 'chocolate donut', 'cupcakes', 'fruit tart', 'raspberry parfait',
-    'strawberry shortcake', 'turtle brownie'
-];
+var word ="";
+var dashArr=[];
+// var choices = ['apple pie', 'blueberry galette', 'chocolate donut', 'cupcakes', 'fruit tart', 'raspberry parfait',
+//     'strawberry shortcake', 'turtle brownie'
+// ];
+var choices = ["apple", "blueberry","chocolate"];
 //randomly select word
+chooseWord();
 function chooseWord() {
-    return Math.floor(Math.random() * choices.length);
+    var rand = Math.floor(Math.random() * choices.length);
+    word = choices[rand];
+    console.log(word);
+    makeDashes(word); 
 }
-var word = choices[chooseWord()];
-console.log(word);
-//split word into characters
-var wordSplit = word.split("");
-return wordSplit;
-//replace characters with dashes
-function makeDashes() {
-    var dashArr = [];
-    for (i = 0; i <= wordSplit.length; i++) {
-        var dash = ["_"];
+
+function makeDashes(dashword){
+    var len = dashword.length;
+    console.log(len);
+        for (var i = 0; i < len; i++) {
+        var dash = "_";
         dashArr.push(dash);
     }
-    //display hidden word
-    rightWordText.TextContent = dashArr;
-    return dashArr;
 
 }
+//split word into characters
+// var wordSplit = word.split("");
+// return wordSplit;
+// //replace characters with dashes
+// function makeDashes() {
+//     var dashArr = [];
+//     for (i = 0; i <= wordSplit.length; i++) {
+//         var dash = ["_"];
+//         dashArr.push(dash);
+//     }
+//     //display hidden word
+//     rightWordText.TextContent = dashArr;
+//     return dashArr;
+
+//}
 
 
 
@@ -34,9 +49,9 @@ function makeDashes() {
 
 
 // user guesses a letter
-document.onkeyup = function (guess) {
-    for (i = 0; i <= letters.length; i++)
 
-}
+// document.onkeyup = function (guess) {
+//     for (i = 0; i <= letters.length; i++)
 
-//
+// }
+// //
