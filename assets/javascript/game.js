34b0243d4aa7da9c8e53cf6c6183c1ba742e591c
Margaret Rumble <megrumble@gmail.com>
2017-09-14 10:30:21 -0400

    used div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,88 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var guess = ""; //guess a letter
-var display = ""; //correct letter-displayed
-var wrong = 0; //wrong guesses
-var lives = 6; //lives
-var word ="";
-var dashArr=[];
+var guess; //guessed letter
+var correct; //correct guess
+var lives; //lives
+var counter; // count correct guesses
+var word = "";
+var len = "";
+var dash = " _ ";
+var currentWordArr = [];
 // var choices = ['apple pie', 'blueberry galette', 'chocolate donut', 'cupcakes', 'fruit tart', 'raspberry parfait',
 //     'strawberry shortcake', 'turtle brownie'
 // ];
-var choices = ["apple", "blueberry","chocolate"];
+var choices = ["apple", "blueberry", "chocolate"];
+//get elements
+var showLives=document.getElementById("my lives");
+
 //randomly select word
 chooseWord();
+
 function chooseWord() {
     var rand = Math.floor(Math.random() * choices.length);
     word = choices[rand];
+    len = word.length;
     console.log(word);
-    makeDashes(word); 
+    makeDashes();
 }
+//replace characters with dashes
+function makeDashes() {
 
-function makeDashes(dashword){
-    var len = dashword.length;
-    console.log(len);
-        for (var i = 0; i < len; i++) {
-        var dash = "_";
-        dashArr.push(dash);
+    for (var i = 0; i < len; i++) {
+       
+        currentWordArr.push(dash);
+        var newDiv = $("<div class='letter'>");
+        newDiv.text(" _ ");
+        $("#wordDiv").append(newDiv);
     }
+    console.log(currentWordArr);
 
 }
-//split word into characters
-// var wordSplit = word.split("");
-// return wordSplit;
-// //replace characters with dashes
+//create guesses
+counter=6;
+$(document).on("keypress", function (event) {
+    $("#wordDiv").empty();
+   var userKey = event.key;
+ //check if guesses letter is correct/ place it correctly  
+    for (var i = 0; i < len; i++) {
+        if ( userKey === word[i])
+        {
+            currentWordArr[i] = userKey;
+            var newDiv = $("<div class='letter'>");
+            newDiv.text(currentWordArr[i]);
+            $("#wordDiv").append(newDiv);
+
+        }  
+        else if ( currentWordArr[i] != dash ){
+            var newDiv = $("<div class='letter'>");
+            newDiv.text(currentWordArr[i]);
+            $("#wordDiv").append(newDiv);
+
+        }
+        else{
+            var newDiv = $("<div class='letter'>");
+            newDiv.text(dash);
+            $("#wordDiv").append(newDiv);
+            counter=counter-1;
+            
+        }
+    }
+    console.log(currentWordArr);
+console.log(counter);
+
+$("#myLives").append(counter);
+
+});
+
+
 // function makeDashes() {
-//     var dashArr = [];
+//     var currentWordArr = [];
 //     for (i = 0; i <= wordSplit.length; i++) {
 //         var dash = ["_"];
-//         dashArr.push(dash);
+//         currentWordArr.push(dash);
 //     }
 //     //display hidden word
-//     rightWordText.TextContent = dashArr;
-//     return dashArr;
+//     rightWordText.TextContent = currentWordArr;
+//     return currentWordArr;
 
 //}
 
